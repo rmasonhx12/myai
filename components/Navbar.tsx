251f9ebebc2a29b64cc6f2bea59d0009e798b722
Robert Hendricks <rmasonh22@gmail.com>
2025-05-24 23:58:26 -0400

    change to blue gradient on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,8 @@
 import Link from "next/link";
 const Navbar = () => {
     return (
-         <div className="bg-black text-white flex justify-between items-center py-5 px-4 md:px-8">
+         <div className="bg-gradient-to-r from-blue-900 to-blue-300 text-white flex justify-between items-center py-5 px-4 md:px-8">
+            
             <div>
                 <Link href="/" className="text-lg font-semibold">
                 myAI
