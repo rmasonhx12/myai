48d93c658ad3defa0087a66c9969a1c521eafbb1
Robert Hendricks <rmasonh22@gmail.com>
2025-05-24 23:43:43 -0400

    build navbar with tailwind

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,16 @@
 // navbar component
+import Link from "next/link";
 const Navbar = () => {
-    return <div>Navbar </div>
+    return (
+         <div className="bg-black text-white flex justify-between items-center py-5 px-4 md:px-8">
+            <div>
+                <Link href="/" className="text-lg font-semibold">
+                myAI
+                </Link>
+                </div>
+                <div>Sign In</div>
+             </div>
+    );
 };
 
 export default Navbar;
