17155647245414668a08f4e5a790cfa19d3e6d06
Robert Hendricks <rmasonh22@gmail.com>
2025-05-24 22:37:20 -0400

    initial setup for template

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// home page
+
+import Navbar from "@/components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
---
+++ components/Navbar.tsx
@@ -0,0 +1,6 @@
+// navbar component
+const Navbar = () => {
+    return <div>Navbar </div>
+};
+
+export default Navbar;
